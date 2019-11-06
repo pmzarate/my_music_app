551749376963a9dffb4dc072891b8b1138b23393
pmzarate <patriciamarakzarate@yahoo.com>
2019-11-05 21:21:39 -0600

    initialized music app, we just gettin started!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-musicHeader">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        THIS IS OUR MUSIC APP--- BITCHES!!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
